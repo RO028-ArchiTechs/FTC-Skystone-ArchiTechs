59720c51eeb0cbff72c70b0f48ee02d1113a8ca1
Rapeanu George <alexandrurapeanu@yahoo.com>
2020-01-14 19:58:51 +0200

    adds GeneralController class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GeneralController.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cCompassSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
+
+
+///Base class of various types of controll bindings
+public class GeneralController {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private LinearOpMode cur_opmode; /// refference to the opmode that calls this controller
+    private ColorSensor sColor = null;
+    private DistanceSensor sDist = null;
+    private ModernRoboticsI2cCompassSensor sCompass = null;
+
+    public GeneralController(LinearOpMode opmode){
+        ///setup Hardware
+        cur_opmode = opmode; /// as far as i'm concerned, this should make cur_opmode behave like a C/C++ pointer, not a copy of the object
+        sColor = cur_opmode.hardwareMap.get(ColorSensor.class, "CLR");
+        sDist = cur_opmode.hardwareMap.get(DistanceSensor.class, "CLR");
+        sCompass = cur_opmode.hardwareMap.get(ModernRoboticsI2cCompassSensor.class, "HDG");
+    }
+
+    public double get_left_stick_y() {
+        return cur_opmode.gamepad1.left_stick_y;
+    }
+
+    public double get_left_stick_x() {
+        return cur_opmode.gamepad1.left_stick_x;
+    }
+
+    public double get_right_stick_y() {
+        return cur_opmode.gamepad1.right_stick_y;
+    }
+
+    public double get_right_stick_x() {
+        return cur_opmode.gamepad1.right_stick_x;
+    }
+
+    public double getDirection() {
+        return (double)sCompass.getDirection();
+    }
+
+    public double red() {
+        return sColor.red();
+    }
+
+    public double green() {
+        return sColor.green();
+    }
+
+    public double blue() {
+        return sColor.blue();
+    }
+
+    public boolean get_x() {
+        return cur_opmode.gamepad1.x;
+    }
+
+    public boolean get_y() {
+        return cur_opmode.gamepad1.y;
+    }
+
+    public boolean get_a() {
+        return cur_opmode.gamepad1.a;
+    }
+
+    public boolean get_b() {
+        return cur_opmode.gamepad1.b;
+    }
+
+    public void reset_runtime() {
+        runtime.reset();
+    }
+
+    public ElapsedTime get_runtime() {
+        return runtime;
+    }
+
+    public boolean get_dpad_up() {
+        return cur_opmode.gamepad1.dpad_up;
+    }
+
+    public boolean get_dpad_down() {
+        return cur_opmode.gamepad1.dpad_down;
+    }
+
+    public Object getDistance(DistanceUnit dist_unit) {
+        return sDist.getDistance(dist_unit);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain.java
@@ -15,6 +15,8 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+import org.firstinspires.ftc.teamcode.GeneralController;
+
 import java.util.Locale;
 
 
@@ -23,18 +25,17 @@ import java.util.Locale;
 public class drivetrain extends LinearOpMode {
 
     // Declare Hardware, timing, etc
-    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor driveFL = null;
     private DcMotor driveFR = null;
     private DcMotor driveRL = null;
     private DcMotor driveRR = null;
-    private Servo  mechGrab = null;
-    private ColorSensor sColor = null;
-    private DistanceSensor sDist = null;
-    private ModernRoboticsI2cCompassSensor sCompass = null;
+    private Servo mechGrab = null;
+
 
     @Override
     public void runOpMode() {
+        ///setup controller
+        GeneralController controller = new GeneralController(this);
 
         //Setup Hardware
         driveFL = hardwareMap.get(DcMotor.class, "FL");
@@ -42,9 +43,6 @@ public class drivetrain extends LinearOpMode {
         driveRL = hardwareMap.get(DcMotor.class, "RL");
         driveRR = hardwareMap.get(DcMotor.class, "RR");
         mechGrab = hardwareMap.get(Servo.class, "GR");
-        sColor = hardwareMap.get(ColorSensor.class, "CLR");
-        sDist = hardwareMap.get(DistanceSensor.class, "CLR");
-        sCompass = hardwareMap.get(ModernRoboticsI2cCompassSensor.class, "HDG");
 
 
         driveFL.setDirection(DcMotor.Direction.FORWARD);
@@ -54,7 +52,7 @@ public class drivetrain extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
+        controller.reset_runtime();
 
         //INPUT
         double vX, vY, vR;     //robot speed as commanded by driver (can be in either LOCAL or GLOBAL coordinates, depending on the MODE)
@@ -82,28 +80,29 @@ public class drivetrain extends LinearOpMode {
 
                 power_RL, power_RR;
 
+
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             //READ INPUTS
-            vY = -gamepad1.left_stick_y;
-            vX = gamepad1.left_stick_x;
-            vR = gamepad1.right_stick_x;
+            vY = -controller.get_left_stick_y();
+            vX = controller.get_left_stick_x();
+            vR = controller.get_right_stick_x();
 
-            A = A0 + Math.toRadians((double)sCompass.getDirection());
+            A = A0 + Math.toRadians(controller.getDirection());
 
-            cR = sColor.red();
-            cG = sColor.green();
-            cB = sColor.blue();
+            cR = controller.red();
+            cG = controller.green();
+            cB = controller.blue();
 
-
-            if (runtime.seconds()-delay > .3){
-                if(gamepad1.x){
+            if (controller.get_runtime().seconds()-delay > .3){
+                if(controller.get_x()){
                     GEAR ++;
 
                 } else if( GEAR > 4) {
                     GEAR = 1;
                 }
-                if(gamepad1.y) {
+                if(controller.get_y()) {
                     if(MODE == 0){
                         MODE = 1;
                     }
@@ -111,23 +110,17 @@ public class drivetrain extends LinearOpMode {
                         MODE = 0;
                     }
                 }
-                if(gamepad1.x && gamepad1.b){
+                if(controller.get_x() && controller.get_b()){
                     A0 = A;
                 }
-                delay = runtime.seconds();
+                delay = controller.get_runtime().seconds();
             }
 
-            if(gamepad1.dpad_up){
+            if(controller.get_dpad_up()){
                 mechGrab.setPosition(0);
-            } else if (gamepad1.dpad_down) {
+            } else if (controller.get_dpad_down()) {
                 mechGrab.setPosition(1);
             }
-
-
-
-
-
-
             switch (GEAR){
                 case 1 : {
                     Gear_shift = .25;
@@ -179,21 +172,20 @@ public class drivetrain extends LinearOpMode {
             driveFR.setPower(power_FR);
             driveRL.setPower(power_RL);
             driveRR.setPower(power_RR);
-            
+
 
             // Telemetry feedback
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Run Time: " + controller.get_runtime().toString());
             telemetry.addData("MODE", ": (%d)", MODE);
             telemetry.addData("GEAR", ": %.2f", Gear_shift);
             telemetry.addData("Distance (cm)",
-                    String.format(Locale.US, "%.02f", sDist.getDistance(DistanceUnit.CM)));
+                    String.format(Locale.US, "%.02f", controller.getDistance(DistanceUnit.CM)));
             telemetry.addData("Color", "\nR%.2f\nG%.2f\nB%.2f", cR, cG, cB);
             telemetry.addData("HDG","%.2f",A);
             telemetry.addData("IN","X%.2f Y%.2f",LocalX,LocalY);
             telemetry.addData("Motors", "FL (%.2f), FR (%.2f), RL (%.2f), RR (%.2f)", power_FL, power_FR, power_RL, power_RR);
 
             telemetry.update();
-
         }
     }
 }
